eb7db4f01158025706b2a29bb4c0a7b07b32c51b
JE-Chen(陳冠穎) <zenxcvwait@gmail.com>
2020-10-12 00:17:23 +0800

    code refactoring

--- Module/Abbreviation.py
+++
@@ -1,9 +0,0 @@
-import keyboard
-
-class Addreviation():
-
-    def __init__(self):
-        pass
-
-    def SetAbbreviation(self,source,replacestring):
-        keyboard.add_abbreviation(source,replacestring)
