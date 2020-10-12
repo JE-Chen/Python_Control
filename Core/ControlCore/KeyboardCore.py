3012bad5f1fade1e73bbb1123580b6538a8d8fa6
JE-Chen(陳冠穎) <zenxcvwait@gmail.com>
2020-10-12 09:45:57 +0800

    Delete KeyboardCore.py

--- Core/ControlCore/KeyboardCore.py
+++
@@ -1,9 +0,0 @@
-from Module.CtypeKeyboardControl import CtypeKeyboardControl
-
-class KeyboardCore():
-
-    def __init__(self):
-        try:
-            self.CtypeKeyboardControl = CtypeKeyboardControl()
-        except Exception as Errr:
-            raise Errr
