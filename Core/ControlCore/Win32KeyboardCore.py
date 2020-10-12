af3ded4cac5be9e06871fdc38a00097dee2bf5ff
JE-Chen(陳冠穎) <zenxcvwait@gmail.com>
2020-10-12 11:55:04 +0800

    Delete Win32KeyboardCore.py

--- Core/ControlCore/Win32KeyboardCore.py
+++
@@ -1,9 +0,0 @@
-from Module.Win32CtypeKeyboardControl import CtypeKeyboardControl
-
-class KeyboardCore():
-
-    def __init__(self):
-        try:
-            self.CtypeKeyboardControl = CtypeKeyboardControl()
-        except Exception as Errr:
-            raise Errr
