49c56022a66461e9874d408b0ff6564f6aea23e6
JE-Chen <zenxcvwait@gmail.com>
2021-07-02 13:48:34 +0800

    test fix error

--- test/win32_keyboard_test.py
+++ test/win32_keyboard_test.py
@@ -1,5 +1,9 @@
-from je_auto_control import *
-win32_ctype_keyboard_control.press_key(keyT)
-win32_ctype_keyboard_control.press_key(keyE)
-win32_ctype_keyboard_control.press_key(keyS)
-win32_ctype_keyboard_control.press_key(keyT)
+from je_auto_control import keyT
+from je_auto_control import keyE
+from je_auto_control import keyS
+from je_auto_control import win32_ctype_keyboard_control as keyboard_control
+
+keyboard_control.press_key(keyT)
+keyboard_control.press_key(keyE)
+keyboard_control.press_key(keyS)
+keyboard_control.press_key(keyT)
--- test/win32_mouse_test.py
+++ test/win32_mouse_test.py
@@ -1,7 +1,7 @@
-from je_auto_control import win32_ctype_mouse_control
+from je_auto_control import win32_ctype_mouse_control as mouse_control
 from je_auto_control.mouse.win32_ctype_mouse_control import right
 
-print(win32_ctype_mouse_control.get_pos())
-win32_ctype_mouse_control.set_pos((809, 388))
-win32_ctype_mouse_control.press_mouse(right)
-win32_ctype_mouse_control.release_mouse(right)
+print(mouse_control.get_pos())
+mouse_control.set_pos((809, 388))
+mouse_control.press_mouse(right)
+mouse_control.release_mouse(right)
