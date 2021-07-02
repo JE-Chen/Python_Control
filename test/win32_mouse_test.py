cdf6e34accdfe7c06a21d7030bd6eb51cb9f8b98
JE-Chen <zenxcvwait@gmail.com>
2021-07-02 13:39:10 +0800

    edit test

--- test/win32_mouse_test.py
+++ test/win32_mouse_test.py
@@ -1,4 +1,7 @@
 from je_auto_control import win32_ctype_mouse_control
+from je_auto_control.mouse.win32_ctype_mouse_control import right
 
 print(win32_ctype_mouse_control.get_pos())
 win32_ctype_mouse_control.set_pos((809, 388))
+win32_ctype_mouse_control.press_mouse(right)
+win32_ctype_mouse_control.release_mouse(right)
