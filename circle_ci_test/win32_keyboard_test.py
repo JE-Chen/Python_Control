d7ea2a86888f6539b875b6622f5f7ed5ac69bfca
JE-Chen <zenxcvwait@gmail.com>
2021-07-14 17:00:59 +0800

    test

--- circle_ci_test/win32_keyboard_test.py
+++ circle_ci_test/win32_keyboard_test.py
@@ -1,7 +1,11 @@
+import sys
+
 from je_auto_control import win32_keyT
 from je_auto_control import win32_keyE
 from je_auto_control import win32_keyS
 from je_auto_control import win32_ctype_keyboard_control as keyboard_control
+
+print(sys.platform)
 
 keyboard_control.press_key(win32_keyT)
 keyboard_control.press_key(win32_keyE)
