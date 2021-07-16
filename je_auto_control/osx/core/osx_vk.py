2e6817a07bdf581f7f77bd4db92a72ff7bc1b290
JE-Chen <zenxcvwait@gmail.com>
2021-07-16 02:17:08 -0700

    add osx vk

---
+++ je_auto_control/osx/core/osx_vk.py
@@ -0,0 +1,97 @@
+key_a = key_A = 0x00
+key_s = key_S = 0x01
+key_d = key_D = 0x02
+key_f = key_F = 0x03
+key_h = key_H = 0x04
+key_g = key_G = 0x05
+key_z = key_Z = 0x06
+key_x = key_X = 0x07
+key_c = key_C = 0x08
+key_v = key_V = 0x09
+key_b = key_B = 0x0b
+key_q = key_Q = 0x0c
+key_w = key_W = 0x0d
+key_e = key_E = 0x0e
+key_r = key_R = 0x0f
+key_y = key_Y = 0x10
+key_t = key_T = 0x11
+key_1 = key_exclam= 0x12
+key_2 = key_at = 0x13
+key_3 = key_numbersign = 0x14
+key_4 = key_money = 0x15
+key_6 = key_asciicicum = 0x16
+key_5 = key_percent = 0x17
+key_equal = key_plus = 0x18
+key_9 = key_parenleft = 0x19
+key_7 = key_ampersand = 0x1a
+key_minus = key_underscore = 0x1b
+key_8 = key_asterisk = 0x1c
+key_0 = key_parenright = 0x1d
+key_bracketright = key_braceright = 0x1e
+key_o = 0x1f
+key_u = 0x20
+key_bracketleft = key_braceleft = 0x21
+key_i = 0x22
+key_p = 0x23
+key_l = 0x25
+key_j = 0x26
+key_apostrophe = key_quotedbl = 0x27
+key_k = 0x28
+key_semicolon = key_colon = 0x29
+key_backslash = key_bar = 0x2a
+key_comma = key_less = 0x2b
+key_salsh = key_question = 0x2c
+key_n = 0x2d
+key_m = 0x2e
+key_period = key_greater = 0x2f
+key_grave = key_asciitilde = 0x32
+key_space = 0x31
+key_return = key_newline = key_enter = 0x24
+key_tab = 0x30
+key_backspace = 0x33
+key_esc = 0x35
+key_command = 0x37
+key_shift = 0x38
+key_caps_lock = 0x39
+key_option = key_alt = 0x3a
+key_ctrl = 0x3b
+key_shift_right = 0x3c
+key_option_right = 0x3d
+key_control_right = 0x3e
+key_fn = 0x3f
+key_f17 = 0x40
+key_volume_up = 0x48
+key_volume_down = 0x49
+key_volume_mute = 0x4a
+key_f18 = 0x4f
+key_f19 = 0x50
+key_f20 = 0x5a
+key_f5 = 0x60
+key_f6 = 0x61
+key_f7 = 0x62
+key_f3 = 0x63
+key_f8 = 0x64
+key_f9 = 0x65
+key_f11 = 0x67
+key_f13 = 0x69
+key_f16 = 0x6a
+key_f14 = 0x6b
+key_f10 = 0x6d
+key_f12 = 0x6f
+key_f15 = 0x71
+key_help = 0x72
+key_home = 0x73
+key_pageup = 0x74
+key_delete = 0x75
+key_f4 = 0x76
+key_end = 0x77
+key_f2 = 0x78
+key_pagedown = 0x79
+key_f1 = 0x7a
+key_left = 0x7b
+key_right = 0x7c
+key_down = 0x7d
+key_up = 0x7e
+key_yen = 0x5d
+key_eisu = 0x66
+key_kana = 0x68
