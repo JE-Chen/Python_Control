9a9c972e532560696c824d7190b25b796d588458
JE-Chen <zenxcvwait@gmail.com>
2021-07-17 05:12:16 -0700

    add test

---
+++ test/osx/template_detection_multi_test.py
@@ -0,0 +1,10 @@
+import time
+
+import cv2
+
+from je_auto_control import template_detection
+
+image_data_array = template_detection.find_image_multi("../../test_template.png", draw_image=True)
+
+print(image_data_array[1])
+
