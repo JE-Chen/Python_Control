86ebd7f25f38bdabb8f366270f3320356af89117
JE-Chen <zenxcvwait@gmail.com>
2021-07-17 19:20:17 +0800

    refactor

--- je_auto_control/osx/core/__init__.py
+++ je_auto_control/osx/core/__init__.py
@@ -1,4 +1,2 @@
-from je_auto_control.osx.core.osx_vk import osx_mouse_left
-from je_auto_control.osx.core.osx_vk import osx_mouse_middle
-from je_auto_control.osx.core.osx_vk import osx_mouse_right
-from je_auto_control.osx.core.osx_vk import osx_key_shift
+from je_auto_control.osx.core import osx_vk
+
