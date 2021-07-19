e095762540e87a17dbccbc84d753cb233f54e4be
JE-Chen <zenxcvwait@gmail.com>
2021-07-19 19:10:31 +0800

    test not need show image

--- circle_ci_test/template_detection_multi_test.py
+++ circle_ci_test/template_detection_multi_test.py
@@ -7,6 +7,3 @@ image_data_array = template_detection.find_image_multi("test1.png", detect_thres
 
 print(image_data_array[1])
 
-cv2.imshow("test", image_data_array[0])
-cv2.waitKey(0)
-cv2.destroyAllWindows()
