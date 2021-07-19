e4060484f30881623ee56024d99140584970782b
JE-Chen <zenxcvwait@gmail.com>
2021-07-19 18:58:09 +0800

    fix test error path

--- circle_ci_test/template_detection_test.py
+++ circle_ci_test/template_detection_test.py
@@ -3,7 +3,7 @@ import cv2
 from je_auto_control import template_detection
 
 # detect_threshold 0~1 , 1 is absolute equal
-image_data_array = template_detection.find_image("../../test_template.png", detect_threshold=1, draw_image=True)
+image_data_array = template_detection.find_image("test_template.png", detect_threshold=1, draw_image=True)
 
 print(image_data_array[1])
 print(image_data_array[2])
